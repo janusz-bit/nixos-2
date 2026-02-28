9f1eb4abd2e9caadab70eef9ae3f7d97354d6b5b
janusz-bit <janusz-bit>
2026-02-28 20:44:54 +0000

    Update home.nix

--- users/wsl/home.nix
+++ users/wsl/home.nix
@@ -7,17 +7,20 @@
 {
   home-manager.users.nixos = {
 
-    programs.git = {
-      enable = true;
-      settings = {
-        user.name = "janusz-bit";
-        user.email = "janusz-bit@proton.me";
-        init.defaultBranch = "main";
+    programs = {
+      gh.enable = true;
+      git = {
+        enable = true;
+        settings = {
+          user.name = "janusz-bit";
+          user.email = "janusz-bit@proton.me";
+          init.defaultBranch = "main";
+        };
+
       };
     };
-    programs.gh.enable = true;
 
   };
   home-manager.backupFileExtension = "backup";
-  home-manager.users.nixos.home.stateVersion = "25.05";
+  home-manager.users.nixos.home.stateVersion = "26.05";
 }
