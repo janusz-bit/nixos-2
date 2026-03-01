403c93aa9a1685529f244627f9089e6e00b7f35f
dinosaur <janusz-bit>
2026-03-01 01:40:29 +0100

    Added and implemented flake parts

--- flake.nix
+++ flake.nix
@@ -6,28 +6,54 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      nixos-wsl,
-      home-manager,
-      ...
-    }:
-    {
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            nixos-wsl.nixosModules.default
-            ./configuration.nix
-            ./wsl.nix
-            home-manager.nixosModules.default
-            ./users/wsl/home.nix
-          ];
+    inputs:
+    # https://flake.parts/module-arguments.html
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        config,
+        withSystem,
+        moduleWithSystem,
+        ...
+      }:
+      {
+        imports = [
+          # Optional: use external flake logic, e.g.
+          # inputs.foo.flakeModules.default
+        ];
+        flake = {
+          # Put your original flake attributes here.
+          nixosConfigurations = {
+            nixos = inputs.nixpkgs.lib.nixosSystem {
+              system = "x86_64-linux";
+              modules = [
+                inputs.nixos-wsl.nixosModules.default
+                ./configuration.nix
+                ./wsl.nix
+                inputs.home-manager.nixosModules.default
+                ./users/wsl/home.nix
+              ];
+            };
+          };
         };
-      };
-    };
+        systems = [
+          # systems for which you want to build the `perSystem` attributes
+          "x86_64-linux"
+          # ...
+        ];
+        perSystem =
+          { config, pkgs, ... }:
+          {
+            # Recommended: move all package definitions here.
+            # e.g. (assuming you have a nixpkgs input)
+            # packages.foo = pkgs.callPackage ./foo/package.nix { };
+            # packages.bar = pkgs.callPackage ./bar/package.nix {
+            #   foo = config.packages.foo;
+            # };
+          };
+      }
+    );
 }
